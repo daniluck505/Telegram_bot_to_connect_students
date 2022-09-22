60e0cfca784130c47bb6a8a748815f1732c8722d
daniluck505 <90381413+daniluck505@users.noreply.github.com>
2022-09-22 18:37:40 +0300

    Create config.py

---
+++ config.py
@@ -0,0 +1,24 @@
+import datetime
+
+TOKEN = 'TOKEN'
+EXEL = 'id'
+DUMP_ID = 'id'
+SUPPORT_ID = 'id'
+all_profiles_chat = 'id'
+admin_id = 'id'
+admins_list = ['id']
+limit_send = 10
+hour_scheduler = 3
+minute_scheduler = 1
+
+offset = datetime.timedelta(hours=3)
+tz = datetime.timezone(offset, name='МСК')
+
+
+# all text
+t1 = 'text'
+t2 = 'text'
+t3 = 'text'
+t4 = 'text'
+t5 = 'text'
+t6 = 'text'
